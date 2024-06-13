a1ace7d79acbe8871e2fa289d7f3f465b047f403
Flavio Mesquita Marinho Filho <flaviomesquitamarinho13@gmail.com>
2024-06-13 20:45:07 -0300

    Criado a View do Vendedor

---
+++ VendasWebMvc/Controllers/VendedoresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendasWebMvc.Data;
+
+namespace VendasWebMvc.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        private readonly VendasWebMvcContext _context;
+
+        public VendedoresController(VendasWebMvcContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
